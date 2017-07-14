d1559ccc063a2037f4dfac202733306e489cc9a0
Nilesh-Bhor <nilesh.bhor@cennest.com>
2017-07-14 17:17:17 +0530

    updated BaseURL

--- KidCheckTest/Helper/AppConstant.cs
+++ KidCheckTest/Helper/AppConstant.cs
@@ -9,8 +9,8 @@ namespace KidCheckTest.Helper
     class AppConstant
     {
         public const int SleepTime = 500;
-        private const string ServerURL = "https://localhost";
-        public static string SignUpURL = string.Format("{0}/signup/", ServerURL);
-        public static string SignInURL = string.Format("{0}/signin.aspx", ServerURL);
+        private const string BaseURL = "https://localhost";
+        public static string SignUpURL = string.Format("{0}/signup/", BaseURL);
+        public static string SignInURL = string.Format("{0}/signin.aspx", BaseURL);
     }
 }
